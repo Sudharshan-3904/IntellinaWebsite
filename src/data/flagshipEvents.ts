c2624d9979c915b2f7e83ef0968b128cd372656b
AkshayKarthickMS <117292543+AkshayKarthickMS@users.noreply.github.com>
2025-03-30 21:02:38 +0530

    Update flagshipEvents.ts

--- src/data/flagshipEvents.ts
+++ src/data/flagshipEvents.ts
@@ -9,7 +9,7 @@ export const flagshipEvents = [
         time: '10:30 AM  - 01:30 PM',
         image: 'https://res.cloudinary.com/dxcctqlbr/image/upload/v1741526463/IMG-20250307-WA0013_sgd1sw.jpg',
         rules: ['Team size: 4','Participants must use OpenAI GPT models.', 'Judging is based on creativity, efficiency, and accuracy.', 'Submissions must be original and unpublished.'],
-        registrationLink: 'https://unstop.com/p/gptathon-coimbatore-institute-of-technology-cit-coimbatore-1430549',
+        registrationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSet-4b9bKx5iPWpNoToc79gFJGu7iMxBAE--aBnq9SGhJgdgg/viewform?usp=dialog',
         prize: 'Exciting cash prizes and e-certificates for winners!',
         prize:' ₹5,000' ,
         venue: 'LBCH, IT-213',
